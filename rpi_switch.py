eb910564aa54ce1f0845b48d20097e4c1adda708
Yoonseop Kang <e0engoon@gmail.com>
2014-12-10 18:46:29 +0900

    refactor rpi_switch.py

--- rpi_switch.py
+++ rpi_switch.py
@@ -6,24 +6,34 @@ import RPi.GPIO as GPIO
 from private import EC2_URL
 
 
+def send_status(status_id):
+    url = EC2_URL + ('/register/%d' % status_id)
+    urllib2.urlopen(url)
+
+
+def is_locked(channel):
+    return GPIO.input(channel)
+
+
 def print_msg(channel):
-    try:
-        if GPIO.input(channel):
-            urllib2.urlopen(EC2_URL + '/register/0')
-            print 'Door Locked'
-        else:
-            urllib2.urlopen(EC2_URL + '/register/1')
-            print 'Door Open'
-    except urllib2.HTTPError:
-        print "Failed to push door status to server"
+    if is_locked(channel):
+        send_status(0)
+        print 'Door Locked'
+    else:
+        send_status(1)
+        print 'Door Open'
 
 
 if __name__ == '__main__':
     print 'Sending switch status to %s' % EC2_URL
 
+    channel = 29
+
+    is_locked(channel)
+
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(29, GPIO.BOTH, callback=print_msg)
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(channel, GPIO.BOTH, callback=print_msg)
 
     while True:
         pass
