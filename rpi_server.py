1f9aff89bacfcb8d7240fe2dcd9c9f6ea07e7717
Yoonseop Kang <e0engoon@gmail.com>
2014-12-10 09:22:19 +0000

    add a small flask web server for rpi_switch.py

---
+++ rpi_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+
+IS_AVAILABLE = None
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    if IS_AVAILABLE is None:
+        return "Not installed yet"
+    elif IS_AVAILABLE:
+        return "Door is open"
+    else:
+        return "Door is locked"
+
+
+@app.route('/register/<int:status>')
+def set_status(status):
+    global IS_AVAILABLE
+    if status == 1:
+        IS_AVAILABLE = True
+    elif status == 0:
+        IS_AVAILABLE = False
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
