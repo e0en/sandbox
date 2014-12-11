43f29760a85a3834f759ab297822e0d513592b97
Yoonseop Kang <e0engoon@gmail.com>
2014-12-11 11:56:34 +0900

    remove print, send time with door status

--- rpi_switch.py
+++ rpi_switch.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib2
+from datetime import datetime
+
 import RPi.GPIO as GPIO
 
 from private import EC2_URL
 
 
 def send_status(status_id):
-    url = EC2_URL + ('/register/%d' % status_id)
+    time_str = datetime.utcnow().isoformat()
+    url = EC2_URL + ('/register/%d/%s' % (status_id, time_str))
+    print url
     urllib2.urlopen(url)
 
 
@@ -18,10 +22,8 @@ def is_locked(channel):
 def print_msg(channel):
     if is_locked(channel):
         send_status(0)
-        print 'Door Locked'
     else:
         send_status(1)
-        print 'Door Open'
 
 
 if __name__ == '__main__':
